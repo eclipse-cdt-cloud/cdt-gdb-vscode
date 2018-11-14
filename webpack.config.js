b4550a755bbcb870e0a50bfef285a78503597f13
Doug Schaefer <dschaefer@blackberry.com>
2018-11-14 10:43:09 -0500

    Remove submodule and adjust build system.
    We now expect the adapter to be checked otu somewhere and we load
    in the webpack config from there and merge it with ours.
    See the README file for details.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,45 @@
 const path = require('path');
+const fs = require('fs');
 
-module.exports = {
-    target: 'node',
-    mode: 'none',
-    context: __dirname,
-    resolve: {
-        extensions: [ '.ts', '.js' ]
-    },
-    entry: {
-        gdbDebugAdapter: './adapter/src/debugAdapter.ts',
-        extension: './src/extension.ts'
-    },
-    output: {
-        path: path.resolve(__dirname, 'out'),
-        filename: '[name].js',
-        libraryTarget: 'commonjs',
-        devtoolModuleFilenameTemplate: '[absolute-resource-path]'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-            }
+let adapterDir = process.env['CDT_ADAPTER_DIR'] || '../cdt-gdb-adapter';
+if (!fs.existsSync(adapterDir)) {
+    adapterDir = 'node_modules/cdt-gdb-adapter';
+}
+adapterConfig = require(path.join(adapterDir, 'webpack.config.js'));
+
+adapterConfig.forEach((config) => {
+    config.output.path = path.resolve(__dirname, 'out');
+});
+
+module.exports = [
+    ...adapterConfig,
+    {
+        target: 'node',
+        mode: 'none',
+        context: __dirname,
+        resolve: {
+            extensions: [ '.ts', '.js' ]
+        },
+        entry: {
+            extension: './src/extension.ts'
+        },
+        output: {
+            path: path.resolve(__dirname, 'out'),
+            filename: '[name].js',
+            libraryTarget: 'commonjs',
+            devtoolModuleFilenameTemplate: '[absolute-resource-path]'
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                }
+            ]
+        },
+        externals: [
+            'vscode'
         ]
-    },
-    externals: [
-        'vscode'
-    ]
-};
+    }
+];
