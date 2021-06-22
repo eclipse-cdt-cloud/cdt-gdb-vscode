71297a1845db9d48a463b45d58163ed786026562
Alice Trifu <alice.trifu@cyberthorstudios.com>
2021-06-22 11:36:32 +0300

    deleted not needed index.ts

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export {MemoryServer} from './memory/server/MemoryServer';
-export * from './memory/client/MemoryBrowser';
-export * from './memory/client/MessageBroker';
-export * from './memory/client/index'
-export * from './extension'; 
