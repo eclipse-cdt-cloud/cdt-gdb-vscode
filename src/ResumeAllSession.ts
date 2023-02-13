2fe9d881c5c65651a1de039eac10858186bf83a8
trongle0504 <104537989+trongle0504@users.noreply.github.com>
2023-02-13 22:28:43 +0700

    Add resume all button and custom debug command (#80)
    Adds a new resume all button to the debug toolbar
    when the amalgamator is the active debug session
    and passes the new custom command to the
    amalgamator for resume all: `cdt-amalgamator/resumeAll`

---
+++ src/ResumeAllSession.ts
@@ -0,0 +1,24 @@
+import { ExtensionContext } from 'vscode';
+import * as vscode from 'vscode';
+
+export class ResumeAllSession {
+    constructor(context: ExtensionContext) {
+        this.registerCommands(context);
+    }
+
+    private readonly registerCommands = (context: ExtensionContext) => {
+        context.subscriptions.push(
+            vscode.commands.registerCommand(
+                'cdt.debug.resumeAllSession',
+                async () => {
+                    const session = vscode.debug.activeDebugSession;
+                    if (session) {
+                        await session.customRequest(
+                            'cdt-amalgamator/resumeAll'
+                        );
+                    }
+                }
+            )
+        );
+    };
+}
